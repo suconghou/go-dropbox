0d500a3e15951b966833881e189a1d936ca99d3c
suconghou <1126045770@qq.com>
2018-09-23 15:19:51 +0800

    init

---
+++ client.go
@@ -0,0 +1,301 @@
+package dropbox
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+	"syscall"
+	"time"
+)
+
+// Client dropbox client
+type Client struct {
+	*Config
+	Files *Files
+}
+
+// File implements an io.ReadWriteCloser for Dropbox files.
+type File struct {
+	Name    string
+	closed  bool
+	writing bool
+	reader  io.ReadCloser
+	pipeR   *io.PipeReader
+	pipeW   *io.PipeWriter
+	c       *Client
+}
+
+// download the file.
+func (f *File) download() error {
+	out, err := f.c.Files.Download(&DownloadInput{f.Name})
+	if err != nil {
+		if strings.HasPrefix(err.Error(), "path/not_found/") {
+			return &os.PathError{Op: "open", Path: f.Name, Err: syscall.ENOENT}
+		}
+		return err
+	}
+
+	f.reader = out.Body
+	return nil
+}
+
+// Read implements io.Reader
+func (f *File) Read(b []byte) (int, error) {
+	if f.reader == nil {
+		if err := f.download(); err != nil {
+			return 0, err
+		}
+	}
+	return f.reader.Read(b)
+}
+
+// Write implements io.Writer.
+func (f *File) Write(b []byte) (int, error) {
+	if !f.writing {
+		f.writing = true
+		go func() {
+			_, err := f.c.Files.Upload(&UploadInput{
+				Mode:   WriteModeOverwrite,
+				Path:   f.Name,
+				Mute:   true,
+				Reader: f.pipeR,
+			})
+
+			f.pipeR.CloseWithError(err)
+		}()
+	}
+	return f.pipeW.Write(b)
+}
+
+// Close implements io.Closer.
+func (f *File) Close() error {
+	if f.closed {
+		return &os.PathError{Op: "close", Path: f.Name, Err: syscall.EINVAL}
+	}
+	f.closed = true
+	if f.writing {
+		if err := f.pipeW.Close(); err != nil {
+			return err
+		}
+	}
+	if f.reader != nil {
+		if err := f.reader.Close(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// FileInfo wraps Dropbox file MetaData to implement os.FileInfo.
+type FileInfo struct {
+	meta *Metadata
+}
+
+// Name of the file.
+func (f *FileInfo) Name() string {
+	return f.meta.Name
+}
+
+// Size of the file.
+func (f *FileInfo) Size() int64 {
+	return int64(f.meta.Size)
+}
+
+// IsDir returns true if the file is a directory.
+func (f *FileInfo) IsDir() bool {
+	return f.meta.Tag == "folder"
+}
+
+// Sys is not implemented.
+func (f *FileInfo) Sys() interface{} {
+	return nil
+}
+
+// ModTime returns the modification time.
+func (f *FileInfo) ModTime() time.Time {
+	return f.meta.ServerModified
+}
+
+// Mode returns the file mode flags.
+func (f *FileInfo) Mode() os.FileMode {
+	var m os.FileMode
+	if f.IsDir() {
+		m |= os.ModeDir
+	}
+	return m
+}
+
+// New dropbox client
+func New(token string) *Client {
+	c := &Client{Config: NewConfig(token)}
+	c.Files = &Files{c}
+	return c
+}
+
+func (c *Client) call(path string, in interface{}) (io.ReadCloser, error) {
+	url := "https://api.dropboxapi.com/2" + path
+	body, err := json.Marshal(in)
+	if err != nil {
+		return nil, err
+	}
+	req, err := http.NewRequest("POST", url, bytes.NewReader(body))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Authorization", "Bearer "+c.AccessToken)
+	req.Header.Set("Content-Type", "application/json")
+	r, _, err := c.do(req)
+	return r, err
+}
+
+// download style endpoint.
+func (c *Client) download(path string, in interface{}, r io.Reader) (io.ReadCloser, int64, error) {
+	url := "https://content.dropboxapi.com/2" + path
+	body, err := json.Marshal(in)
+	if err != nil {
+		return nil, 0, err
+	}
+	req, err := http.NewRequest("POST", url, r)
+	if err != nil {
+		return nil, 0, err
+	}
+	req.Header.Set("Authorization", "Bearer "+c.AccessToken)
+	req.Header.Set("Dropbox-API-Arg", string(body))
+	if r != nil {
+		req.Header.Set("Content-Type", "application/octet-stream")
+	}
+	return c.do(req)
+}
+
+func (c *Client) do(req *http.Request) (io.ReadCloser, int64, error) {
+	res, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return nil, 0, err
+	}
+	if res.StatusCode < 400 {
+		return res.Body, res.ContentLength, err
+	}
+	defer res.Body.Close()
+	e := &Error{
+		Status:     http.StatusText(res.StatusCode),
+		StatusCode: res.StatusCode,
+	}
+	kind := res.Header.Get("Content-Type")
+	if strings.Contains(kind, "text/plain") {
+		if b, err := ioutil.ReadAll(res.Body); err == nil {
+			e.Summary = string(b)
+			return nil, 0, e
+		}
+		return nil, 0, err
+	}
+	if err := json.NewDecoder(res.Body).Decode(e); err != nil {
+		return nil, 0, err
+	}
+	return nil, 0, e
+}
+
+// Stat returns file and directory meta-data for `name`.
+func (c *Client) Stat(name string) (os.FileInfo, error) {
+	out, err := c.Files.GetMetadata(&GetMetadataInput{
+		Path: name,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return &FileInfo{&out.Metadata}, nil
+}
+
+// ListN returns entries in dir `name`. Up to `n` entries, or all when `n` <= 0.
+func (c *Client) ListN(name string, n int) (list []os.FileInfo, err error) {
+	var cursor string
+	if n <= 0 {
+		n = -1
+	}
+	for {
+		var out *ListFolderOutput
+		if cursor == "" {
+			if out, err = c.Files.ListFolder(&ListFolderInput{Path: name}); err != nil {
+				return
+			}
+			cursor = out.Cursor
+		} else {
+			if out, err = c.Files.ListFolderContinue(&ListFolderContinueInput{cursor}); err != nil {
+				return
+			}
+			cursor = out.Cursor
+		}
+		if err != nil {
+			return
+		}
+		for _, ent := range out.Entries {
+			list = append(list, &FileInfo{ent})
+		}
+		if n >= 0 && len(list) >= n {
+			list = list[:n]
+			break
+		}
+		if !out.HasMore {
+			break
+		}
+	}
+	if n >= 0 && len(list) == 0 {
+		err = io.EOF
+		return
+	}
+	return
+}
+
+// List returns all entries in dir `name`.
+func (c *Client) List(name string) ([]os.FileInfo, error) {
+	return c.ListN(name, 0)
+}
+
+// ListFilter returns all entries in dir `name` filtered by `filter`.
+func (c *Client) ListFilter(name string, filter func(info os.FileInfo) bool) (ret []os.FileInfo, err error) {
+	ents, err := c.ListN(name, 0)
+	if err != nil {
+		return
+	}
+	for _, ent := range ents {
+		if filter(ent) {
+			ret = append(ret, ent)
+		}
+	}
+	return
+}
+
+// ListFolders returns all folders in dir `name`.
+func (c *Client) ListFolders(name string) ([]os.FileInfo, error) {
+	return c.ListFilter(name, func(info os.FileInfo) bool {
+		return info.IsDir()
+	})
+}
+
+// ListFiles returns all files in dir `name`.
+func (c *Client) ListFiles(name string) ([]os.FileInfo, error) {
+	return c.ListFilter(name, func(info os.FileInfo) bool {
+		return !info.IsDir()
+	})
+}
+
+// Open returns a File for reading and writing.
+func (c *Client) Open(name string) *File {
+	r, w := io.Pipe()
+	return &File{
+		Name:  name,
+		c:     c,
+		pipeR: r,
+		pipeW: w,
+	}
+}
+
+// Read returns the contents of `name`.
+func (c *Client) Read(name string) ([]byte, error) {
+	f := c.Open(name)
+	defer f.Close()
+	return ioutil.ReadAll(f)
+}
---
+++ files.go
@@ -0,0 +1,202 @@
+package dropbox
+
+import (
+	"encoding/json"
+	"io"
+	"time"
+)
+
+// WriteMode determines what to do if the file already exists.
+type WriteMode string
+
+// Supported write modes.
+const (
+	WriteModeAdd       WriteMode = "add"
+	WriteModeOverwrite           = "overwrite"
+)
+
+// Files do dropbox files manage
+type Files struct {
+	*Client
+}
+
+// Metadata for a file or folder.
+type Metadata struct {
+	Tag            string           `json:".tag"`
+	Name           string           `json:"name"`
+	PathLower      string           `json:"path_lower"`
+	PathDisplay    string           `json:"path_display"`
+	ClientModified time.Time        `json:"client_modified"`
+	ServerModified time.Time        `json:"server_modified"`
+	Rev            string           `json:"rev"`
+	Size           uint64           `json:"size"`
+	ID             string           `json:"id"`
+	MediaInfo      *MediaInfo       `json:"media_info,omitempty"`
+	SharingInfo    *FileSharingInfo `json:"sharing_info,omitempty"`
+	ContentHash    string           `json:"content_hash,omitempty"`
+}
+
+// Dimensions specifies the dimensions of a photo or video.
+type Dimensions struct {
+	Width  uint64 `json:"width"`
+	Height uint64 `json:"height"`
+}
+
+// GPSCoordinates specifies the GPS coordinate of a photo or video.
+type GPSCoordinates struct {
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+}
+
+// PhotoMetadata specifies metadata for a photo.
+type PhotoMetadata struct {
+	Dimensions *Dimensions     `json:"dimensions,omitempty"`
+	Location   *GPSCoordinates `json:"location,omitempty"`
+	TimeTaken  time.Time       `json:"time_taken,omitempty"`
+}
+
+// VideoMetadata specifies metadata for a video.
+type VideoMetadata struct {
+	Dimensions *Dimensions     `json:"dimensions,omitempty"`
+	Location   *GPSCoordinates `json:"location,omitempty"`
+	TimeTaken  time.Time       `json:"time_taken,omitempty"`
+	Duration   uint64          `json:"duration,omitempty"`
+}
+
+// MediaMetadata provides metadata for a photo or video.
+type MediaMetadata struct {
+	Photo *PhotoMetadata `json:"photo,omitempty"`
+	Video *VideoMetadata `json:"video,omitempty"`
+}
+
+// MediaInfo provides additional information for a photo or video file.
+type MediaInfo struct {
+	Pending  bool           `json:"pending"`
+	Metadata *MediaMetadata `json:"metadata,omitempty"`
+}
+
+// FileSharingInfo for a file which is contained in a shared folder.
+type FileSharingInfo struct {
+	ReadOnly             bool   `json:"read_only"`
+	ParentSharedFolderID string `json:"parent_shared_folder_id"`
+	ModifiedBy           string `json:"modified_by,omitempty"`
+}
+
+// GetMetadataInput request input.
+type GetMetadataInput struct {
+	Path             string `json:"path"`
+	IncludeMediaInfo bool   `json:"include_media_info"`
+}
+
+// GetMetadataOutput request output.
+type GetMetadataOutput struct {
+	Metadata
+}
+
+// ListFolderInput request input.
+type ListFolderInput struct {
+	Path             string `json:"path"`
+	Recursive        bool   `json:"recursive"`
+	IncludeMediaInfo bool   `json:"include_media_info"`
+	IncludeDeleted   bool   `json:"include_deleted"`
+}
+
+// ListFolderOutput request output.
+type ListFolderOutput struct {
+	Cursor  string `json:"cursor"`
+	HasMore bool   `json:"has_more"`
+	Entries []*Metadata
+}
+
+// ListFolderContinueInput request input.
+type ListFolderContinueInput struct {
+	Cursor string `json:"cursor"`
+}
+
+// DownloadInput request input.
+type DownloadInput struct {
+	Path string `json:"path"`
+}
+
+// DownloadOutput request output.
+type DownloadOutput struct {
+	Body   io.ReadCloser
+	Length int64
+}
+
+// UploadInput request input.
+type UploadInput struct {
+	Path           string    `json:"path"`
+	Mode           WriteMode `json:"mode"`
+	AutoRename     bool      `json:"autorename"`
+	Mute           bool      `json:"mute"`
+	ClientModified string    `json:"client_modified,omitempty"`
+	Reader         io.Reader `json:"-"`
+}
+
+// UploadOutput request output.
+type UploadOutput struct {
+	Metadata
+}
+
+// GetMetadata returns the metadata for a file or folder.
+func (c *Files) GetMetadata(in *GetMetadataInput) (out *GetMetadataOutput, err error) {
+	body, err := c.call("/files/get_metadata", in)
+	if err != nil {
+		return
+	}
+	defer body.Close()
+	err = json.NewDecoder(body).Decode(&out)
+	return
+}
+
+// ListFolder returns the metadata for a file or folder.
+func (c *Files) ListFolder(in *ListFolderInput) (out *ListFolderOutput, err error) {
+	in.Path = normalizePath(in.Path)
+	body, err := c.call("/files/list_folder", in)
+	if err != nil {
+		return
+	}
+	defer body.Close()
+	err = json.NewDecoder(body).Decode(&out)
+	return
+}
+
+// ListFolderContinue pagenates using the cursor from ListFolder.
+func (c *Files) ListFolderContinue(in *ListFolderContinueInput) (out *ListFolderOutput, err error) {
+	body, err := c.call("/files/list_folder/continue", in)
+	if err != nil {
+		return
+	}
+	defer body.Close()
+	err = json.NewDecoder(body).Decode(&out)
+	return
+}
+
+// Download a file.
+func (c *Files) Download(in *DownloadInput) (out *DownloadOutput, err error) {
+	body, l, err := c.download("/files/download", in, nil)
+	if err != nil {
+		return
+	}
+	out = &DownloadOutput{body, l}
+	return
+}
+
+// Upload a file smaller than 150MB.
+func (c *Files) Upload(in *UploadInput) (out *UploadOutput, err error) {
+	body, _, err := c.download("/files/upload", in, in.Reader)
+	if err != nil {
+		return
+	}
+	defer body.Close()
+	err = json.NewDecoder(body).Decode(&out)
+	return
+}
+
+func normalizePath(s string) string {
+	if s == "/" {
+		return ""
+	}
+	return s
+}
